56ee7050b3e2775c3e0da03b32ac59010c4f5aff
marmar <meriemiag@yahoo.fr>
2024-04-14 03:41:27 +0200

    change data aws_s3_bucket_object to aws_s3_object cause deprecated

--- tts-backend-terraform-code/lambda.tf
+++ tts-backend-terraform-code/lambda.tf
@@ -1,5 +1,5 @@
 # Retrieves information about the Lambda function zip file 
-data "aws_s3_bucket_object" "lambda_zip" {
+data "aws_s3_object" "lambda_zip" {
   bucket = "lambdacode17032024"
   key    = "lambda.zip"
 }
@@ -9,9 +9,9 @@ data "aws_s3_bucket_object" "lambda_zip" {
 resource "aws_lambda_function" "texttospeech" {
   function_name = "texttospeech"
 
-  s3_bucket         = data.aws_s3_bucket_object.lambda_zip.bucket
-  s3_key            = data.aws_s3_bucket_object.lambda_zip.key
-  s3_object_version = data.aws_s3_bucket_object.lambda_zip.version_id
+  s3_bucket         = data.aws_s3_object.lambda_zip.bucket
+  s3_key            = data.aws_s3_object.lambda_zip.key
+  s3_object_version = data.aws_s3_object.lambda_zip.version_id
 
   # The handler must be name_of_lambda_code_file.name_of_your_code_handler 
   handler = "lambda.lambda_handler"
