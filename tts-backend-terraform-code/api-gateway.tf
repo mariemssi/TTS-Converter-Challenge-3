2e9e5e78068f1c5bdc5509012614bb126092de92
marmar <meriemiag@yahoo.fr>
2024-04-16 08:42:14 +0200

    delete cors configuration of the api gateway

--- tts-backend-terraform-code/api-gateway.tf
+++ tts-backend-terraform-code/api-gateway.tf
@@ -3,15 +3,15 @@ resource "aws_apigatewayv2_api" "api_gateway" {
   name          = "http-api"
   protocol_type = "HTTP"
   //this bloc is added to resolve cors pbm
-  cors_configuration {
-    allow_origins = ["*"]
+  //1 cors_configuration {
+    // 2 allow_origins = ["*"]
     //allow_methods = ["POST", "GET", "OPTIONS"]
     //allow_headers = ["content-type"]
-    allow_methods = ["*"]
+    // 3 allow_methods = ["*"]
     //allow_headers = ["Content-Type", "Authorization", "X-Amz-Date", "X-Api-Key", "X-Amz-Security-Token"]
-    allow_headers = ["*"]
-    max_age       = 0
-  }
+    // 4 allow_headers = ["*"]
+    // 5 max_age       = 0
+ //6 }
 
 }
 # Create a stage for the created API Gateway
